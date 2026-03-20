2347cddc275aa685936e05f6c0d2f8dda1f4ba84
Pablo <pablovazquezbarrena97@gmail.com>
2026-03-20 20:07:51 +0100

    fix: tests works!

--- src/test/java/com/planing/diet_service/DietServiceApplicationTests.java
+++ src/test/java/com/planing/diet_service/DietServiceApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.planing.diet_service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DietServiceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.planing.diet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DietServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
