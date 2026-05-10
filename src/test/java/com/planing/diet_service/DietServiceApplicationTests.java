698e680d5499832cfe647ae4ee0407a99d0f251c
Pablo <pablovazquezbarrena97@gmail.com>
2026-05-10 20:24:25 +0200

    test: stabilize backend context smoke test

--- src/test/java/com/planing/diet_service/DietServiceApplicationTests.java
+++ src/test/java/com/planing/diet_service/DietServiceApplicationTests.java
@@ -1,13 +1,15 @@
-//package com.planing.diet_service;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class DietServiceApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.planing.diet_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class DietServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
